4980e1639c21d24abf7dbd8ee554d7448ae67924
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-11-24 00:28:36 +0530

    header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <>
       <Nav />
+      <Header />
     </>
   );
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,7 +1,7 @@
 const Nav = () => {
   return (
-    <div className='flex justify-center p-[4%]'>
-      <p className='font-black text-6xl font-barlow'>THE GREEN CAFE</p>
+    <div className='flex justify-center p-[4%] bg-[url("https://www.shutterstock.com/image-vector/leaf-cannabis-colorfull-logo-illustrations-your-1917651983")]'>
+      <p className='font-black text-5xl font-barlow'>THE GREEN CAFE</p>
     </div>
   );
 };
