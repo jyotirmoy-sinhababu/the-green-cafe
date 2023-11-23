a0e1ce8d5d72980c1d20212f38803ffcae6b5221
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-11-23 17:59:01 +0530

    style added to nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,7 +1,7 @@
 const Nav = () => {
   return (
-    <div className='flex justify-center'>
-      <p>THE GREEN CAFE</p>
+    <div className='flex justify-center p-[4%]'>
+      <p className='font-black text-6xl font-barlow'>THE GREEN CAFE</p>
     </div>
   );
 };
