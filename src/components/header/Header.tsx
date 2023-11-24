75c7e2893f7af30e57a8903aa3be13c4f525f56c
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-11-24 11:44:17 +0530

    browser router installed style added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,13 @@
+import { NavLink } from 'react-router-dom';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='flex justify-center gap-[12%]'>
+      <NavLink to=''>About</NavLink>
+      <NavLink to=''>Food & Drinks</NavLink>
+      <NavLink to=''>Our Location</NavLink>
+    </div>
+  );
 };
 
 export default Header;
