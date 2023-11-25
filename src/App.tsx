0d25f3a26cf18c182cb9abbd20b3a16f3f1e036a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-11-25 11:52:34 +0530

    navlink created in header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
+import LandingPage from './pages/landingPage/LandingPage';
 
 function App() {
   return (
     <>
       <Nav />
       <Header />
+      <LandingPage />
     </>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,16 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className='flex justify-center gap-[12%]'>
-      <NavLink to=''>About</NavLink>
-      <NavLink to=''>Food & Drinks</NavLink>
-      <NavLink to=''>Our Location</NavLink>
+    <div className='flex justify-center gap-[15%]'>
+      <NavLink className='font-barlow text-xl' to=''>
+        About
+      </NavLink>
+      <NavLink className='font-barlow text-xl' to=''>
+        Food & Drinks
+      </NavLink>
+      <NavLink className='font-barlow text-xl' to=''>
+        Our Location
+      </NavLink>
     </div>
   );
 };
